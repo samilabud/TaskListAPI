038fd2ba0b8e77e5c7be33719024a3a8a1ac27b4
Samil Abud <samilabud@gmail.com>
2023-12-18 16:20:50 -0400

    removing on memory task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -15,41 +15,41 @@ import { CreateTaskDto, UpdateTaskDto } from './dto/task.dto';
 export class TasksController {
   constructor(private taskService: TasksService) {}
 
-  @Get()
-  @HttpCode(200)
-  tasks() {
-    return this.taskService.getAllTasks();
-  }
-  @Post()
-  @HttpCode(200)
-  createTask(@Body() newTask: CreateTaskDto) {
-    const { title, description } = newTask;
-    return this.taskService.createTask(title, description);
-  }
-  @Delete(':id')
-  @HttpCode(200)
-  deleteTask(@Param('id') taskId: string) {
-    return this.taskService.deleteTask(taskId);
-  }
-  @Patch(':id')
-  @HttpCode(200)
-  updateTask(@Param('id') id: string, @Body() updateTask: UpdateTaskDto) {
-    const { title, description, status } = updateTask;
-    return this.taskService.updateTask(id, { title, description, status });
-  }
+  // @Get()
+  // @HttpCode(200)
+  // tasks() {
+  //   return this.taskService.getAllTasks();
+  // }
+  // @Post()
+  // @HttpCode(200)
+  // createTask(@Body() newTask: CreateTaskDto) {
+  //   const { title, description } = newTask;
+  //   return this.taskService.createTask(title, description);
+  // }
+  // @Delete(':id')
+  // @HttpCode(200)
+  // deleteTask(@Param('id') taskId: string) {
+  //   return this.taskService.deleteTask(taskId);
+  // }
+  // @Patch(':id')
+  // @HttpCode(200)
+  // updateTask(@Param('id') id: string, @Body() updateTask: UpdateTaskDto) {
+  //   const { title, description, status } = updateTask;
+  //   return this.taskService.updateTask(id, { title, description, status });
+  // }
 
-  @Get('/test')
+  @Get()
   getTasksInMongo() {
     return this.taskService.getAllFromMongoTask();
   }
 
-  @Post('/test')
+  @Post()
   createTaskInMongo() {
     this.taskService.createMongoTask();
     return 'it is working';
   }
 
-  @Patch('/test/:id')
+  @Patch(':id')
   updateTaskInMongo(
     @Param('id') id: string,
     @Body() updateTask: UpdateTaskDto,
@@ -57,7 +57,7 @@ export class TasksController {
     return this.taskService.updateMongoTask(id, updateTask);
   }
 
-  @Delete('/test/:id')
+  @Delete(':id')
   deleteTaskInMongo(@Param('id') id: string) {
     return this.taskService.deleteMongoTask(id);
   }
